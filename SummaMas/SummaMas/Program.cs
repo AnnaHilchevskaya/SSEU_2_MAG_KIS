93159010a23b61418cd47443bd6a9299edb19dfb
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 20:06:13 +0400

    Л3.П1 Суммирование чисел в цикле for

---
+++ SummaMas/SummaMas/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SummaMas
+{
+    class SummaMas
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Введите значение: ");  
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];             
+            }
+            Console.WriteLine("Сумма: " + sum);
+            Console.ReadKey();
+        }
+    }
+}
+
