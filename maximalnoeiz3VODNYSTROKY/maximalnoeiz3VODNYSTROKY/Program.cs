ec814821f74759221e9b27c075c6c4717cf3ec5c
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 19:09:35 +0400

    Л2.П2 Максимальное из трех чисел через операцию ?: в одну строку

---
+++ maximalnoeiz3VODNYSTROKY/maximalnoeiz3VODNYSTROKY/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maximalnoeiz3VODNYSTROKY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ввод исходных значений
+            int A, B, C;
+
+            if (int.TryParse(Console.ReadLine(), out A))
+            {
+                if (int.TryParse(Console.ReadLine(), out B))
+                {
+                    if (int.TryParse(Console.ReadLine(), out C))
+                    {// максимальное значение
+                        int Max = ((Max = (A >= B) ? A : B) >= C) ? Max : C;
+
+
+                        // Вывод результата 
+                        Console.WriteLine(" Максимальное = " + Max);
+                    }
+                    // Завершение программы
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
