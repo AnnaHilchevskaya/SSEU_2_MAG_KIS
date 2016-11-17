622abd5450d1b43abb3c6bd1b628b1fb7424e277
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 19:54:35 +0400

    Л2.П4 Консольный калькулятор: сначала вводим операнды, потом операцию

---
+++ kalkulaytor/kalkulaytor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primer4
+{
+    class Primer4
+    {
+        static void Main(string[] args)
+        {
+            double operand1, operand2; //операнды
+            double result = 0; //результат вычисления
+            string operation; //действие
+            if (double.TryParse(Console.ReadLine(), out operand1) && double.TryParse(Console.ReadLine(), out operand2))
+            {
+                //выбираем операцию
+                operation = Console.ReadLine();
+                switch (operation)
+                {
+                    case "+": result = operand1 + operand2; break;
+                    case "-": result = operand1 - operand2; break;
+                    case "*": result = operand1 * operand2; break;
+                    case "/": result = operand1 / operand2; break;
+                    case "sqrt": result = Math.Sqrt(operand1); break;
+                    default: Console.WriteLine("Неверная операция!"); break;
+                };
+                //выводим результат
+                Console.WriteLine(operand1 + " " + operation + " " + operand2 + " = " + result);
+            }
+            else
+            {
+                Console.WriteLine("Операнд должен быть числом!");
+            };
+            Console.ReadKey();
+        }
+    }
+}
+
