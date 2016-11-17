e93685aecc5a40b32e9a623051d675ab6f28f9bc
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 19:05:18 +0400

    Л2.П2 Максимальное из трех чисел через сравнение третьего с максимальным из первых двух

---
+++ maximalnoeizpervihdvuh/maximalnoeizpervihdvuh/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maximalnoeizpervihdvuh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ввод исходных значений
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
+            {
+                int Max;
+                // если  А больше 
+                if (A >= B)
+                {
+                    Max = A;
+                }
+                // иначе B больше
+                else
+                {
+                    Max = B;
+                };
+                // если С больше максимального из А, В, то С- максимальное
+               if (C >= Max)
+                {
+                    Max = C;
+
+                };
+
+                // Вывод результата 
+                Console.WriteLine(" Максимальное = " + Max);
+            }
+            // Завершение программы
+            Console.ReadKey();
+        }
+    }
+}
