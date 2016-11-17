87c4e22eecea888718850125266e6665b638633b
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 19:08:14 +0400

    Л2.П2 Максимальное из трех чисел через операцию ?:

---
+++ maximalnoeiz3Soperatorom/maximalnoeiz3sternarnimoperatorom/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace maximalnoeiz3sternarnimoperatorom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ввод исходных значений
+            int A, B, C;
+          
+            if (int.TryParse(Console.ReadLine(), out A))
+            {
+                 if (int.TryParse(Console.ReadLine(), out B))
+
+                 { 
+                     if (int.TryParse(Console.ReadLine(), out C))
+          
+            {// максимальное значение
+                int Max = (A >= B) ? A : B;
+                Max = (Max >= C) ? Max : C;
+
+                // Вывод результата 
+                Console.WriteLine(" Максимальное = " + Max);
+            }
+            // Завершение программы
+            Console.ReadKey();
+        }
+    }
+}
+    }
+}
