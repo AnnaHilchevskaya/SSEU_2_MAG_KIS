9a612287b8818fcb8fe94c8a3579e29e13c184bb
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 18:58:29 +0400

    Л2.П2 Максимальное из трех чисел через if с логическим И (&&)

---
+++ Maximum3logI/Maximum3logI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Maximum3logI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ввод исходных значений
+            int A, B, C;
+            if (int.TryParse(Console.ReadLine(), out A) && int.TryParse(Console.ReadLine(), out B) && int.TryParse(Console.ReadLine(), out C))
+            {
+                int Max;
+                // если  А больше остальных
+                if ((A >= B) && (A >= C))
+                {
+                    Max = A;
+                }
+                // иначе, если B больше остальных
+                else if ((B >= A) && (B >= C))
+                {
+                    Max = B;
+                }
+                // иначе - остается С
+                else
+                {
+                    Max = C;
+
+                };
+
+                // Вывод результата 
+                Console.WriteLine(" Максимальное = " + Max);
+            }
+            // Завершение программы
+            Console.ReadKey();
+        }
+    }
+}
