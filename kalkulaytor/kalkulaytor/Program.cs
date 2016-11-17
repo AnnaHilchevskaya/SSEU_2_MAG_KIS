d40d2bcb9d6d6ee44d727752d2d604f65895f041
AnnaHilchevskaya <any4848@yandex.ru>
2016-11-17 19:57:18 +0400

    Л2.П4 Консольный калькулятор: сначала вводим первый операнд, потом операцию, потом второй операнд

--- kalkulaytor/kalkulaytor/Program.cs
+++ kalkulaytor/kalkulaytor/Program.cs
@@ -8,23 +8,52 @@ namespace Primer4
     {
         static void Main(string[] args)
         {
-            double operand1, operand2; //операнды
+            double operand1, operand2 = 0; //операнды
             double result = 0; //результат вычисления
             string operation; //действие
-            if (double.TryParse(Console.ReadLine(), out operand1) && double.TryParse(Console.ReadLine(), out operand2))
+            Console.WriteLine("Введите число: ");
+            if (double.TryParse(Console.ReadLine(), out operand1))
             {
                 //выбираем операцию
+                Console.WriteLine("Выберите действие: ");
+                Console.WriteLine("+ - сложение");
+                Console.WriteLine("- - вычитание");
+                Console.WriteLine("* - умножение");
+                Console.WriteLine("/ - деление");
+                Console.WriteLine("sqrt - корень из числа");
                 operation = Console.ReadLine();
                 switch (operation)
                 {
-                    case "+": result = operand1 + operand2; break;
-                    case "-": result = operand1 - operand2; break;
-                    case "*": result = operand1 * operand2; break;
-                    case "/": result = operand1 / operand2; break;
+                    //операции с двумя операндами
+                    case "+":
+                    case "-":
+                    case "*":
+                    case "/":
+                        //считываем второй операнд
+                        Console.WriteLine("Введите второе значение: ");
+                        if (double.TryParse(Console.ReadLine(), out operand2))
+                        {
+                            //какая именно операция?
+                            switch (operation)
+                            {
+                                case "+": result = operand1 + operand2; break;
+                                case "-": result = operand1 - operand2; break;
+                                case "*": result = operand1 * operand2; break;
+                                case "/": result = operand1 / operand2; break;
+                            };
+                        }
+                        else
+                        {
+                            Console.WriteLine("Операнд должен быть числом!");
+                        };
+                        break;
+                    //операция с одним операндом
                     case "sqrt": result = Math.Sqrt(operand1); break;
+                    //недопустимые операции
                     default: Console.WriteLine("Неверная операция!"); break;
                 };
                 //выводим результат
+                Console.WriteLine("Результат: ");
                 Console.WriteLine(operand1 + " " + operation + " " + operand2 + " = " + result);
             }
             else
@@ -35,4 +64,3 @@ namespace Primer4
         }
     }
 }
-
