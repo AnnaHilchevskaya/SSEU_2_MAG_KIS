419a38767d59f0ee95496f89b261fb59e9894390
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-08 13:49:38 +0400

    Л3 задание 2 сумма цифр числа

---
+++ ConsolesummaChisla/ConsolesummaChisla/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace summaChisla
+{
+    class summachisla
+    {
+        public static int Summa(int a)
+        {// объявляем переменные 
+            int s = 0;
+            while (a > 0)
+            {
+                s = s + (a % 10);
+                a = a / 10;
+            }
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Summa={0}", Summa(a)); //выводим сумму
+            Console.ReadKey();
+        }
+    }
+} 
