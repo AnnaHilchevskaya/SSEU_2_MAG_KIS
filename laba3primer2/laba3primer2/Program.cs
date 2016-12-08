7ef419fba66cd1022dcadacd47a91f48b249fe04
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-08 20:15:20 +0400

    Л3.П2 Поиск индекса максимального значения в массиве

--- laba3primer2/laba3primer2/Program.cs
+++ laba3primer2/laba3primer2/Program.cs
@@ -1,34 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
-namespace ConsoleApplication1
+namespace ArrayMax
 {
-    class Program
+    class ArrayMax
     {
         static void Main(string[] args)
-        {//ввод длины массива
+        {
+            //ввод длины массива
             int n;
             do
             {
-                Console.Write("Введите длину массива:");
-            } while (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
+                Console.Write("Введите длину массива: ");
+            }
+            while (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
             //инициализация массива
+            int[] a = new int[n];
+            //ввод значений массива
+            //поиск максимального значения
             int i = 0;
             while (i < n)
             {
-                Console.Write("Введите а [" + i + "]: ");
+                Console.Write("Введите а[" + i + "]:");
                 if (int.TryParse(Console.ReadLine(), out a[i]))
                 {
                     i++;
                 }
                 else
                 {
-                    Console.WriteLine(" Введите целое число! ");
-                }; ;
+                    Console.WriteLine("Введите целое число!");
+                };
             };
-            // выход из программы
+            //поиск максимального значени
+            int max = int.MinValue;
+            int iMax = 0;
+            for (i = 1; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                    iMax = i;
+                };
+            };
+            //вывод результата
+            Console.WriteLine("Максимальное значение a[" + iMax + "] = " + max);
             Console.ReadKey();
         }
     }
