6b66a81adb24ba0b870cf6ee0868894cb05df4ec
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-08 18:54:51 +0400

    Л3.П2 Поиск максимального значения в массиве

---
+++ laba3primer2/laba3primer2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {//ввод длины массива
+            int n;
+            do
+            {
+                Console.Write("Введите длину массива:");
+            } while (!int.TryParse(Console.ReadLine(), out n) || (n <= 0));
+            //инициализация массива
+            int i = 0;
+            while (i < n)
+            {
+                Console.Write("Введите а [" + i + "]: ");
+                if (int.TryParse(Console.ReadLine(), out a[i]))
+                {
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine(" Введите целое число! ");
+                }; ;
+            };
+            // выход из программы
+            Console.ReadKey();
+        }
+    }
+}
