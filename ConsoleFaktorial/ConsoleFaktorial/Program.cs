477940d08deb3be0f440bda38f53d22c4378344e
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-08 13:08:50 +0400

    Л3 Задание1 Факториал

---
+++ ConsoleFaktorial/ConsoleFaktorial/Program.cs
@@ -0,0 +1,18 @@
+﻿ using System;
+ using System.Collections.Generic;
+ using System.Text;
+
+namespace Faktorial
+ {  
+    class Program  
+    {  
+     static void Main(string[] args)       
+        {   
+        int n = 10;  // количество циклов в факториале          
+        int factorial = 1;   // значение факториала    
+        for (int i = 2; i <= n; i++) // цикл начинаем с 2, т.к. нет смысла начинать с 1     
+             {   factorial = factorial * i;  
+                    }                Console.WriteLine("Факториал 10 " + n + " = " + factorial);             
+                      Console.Read();    
+                         }   
+                    } }
