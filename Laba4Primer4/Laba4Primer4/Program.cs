4c8c078d39d4d4d1fa01c287da041728421089aa
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-09 17:53:26 +0400

    Л4.П4 Сортировка слов по алфавиту

---
+++ Laba4Primer4/Laba4Primer4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Text;
+ 
+ namespace Laba4Primer4
+ {
+     class Program
+     {
+         static void Main(string[] args)
+         {
+             Console.WriteLine("Введите слова, раделенные пробелами");
+             string words=Console.ReadLine();
+             
+             string[]wordsArr=words.Split(' ');
+             Array.Sort(wordsArr);
+             words=wordsArr[0];
+             for(int i=0;i<wordsArr.Length;++i)
+                words=words+' '+wordsArr[i];
+ 
+                 Console.WriteLine("Сортировка слов по алфавиту:");
+                 Console.WriteLine(words);
+                 Console.ReadKey();
+         }
+     }
+ }
