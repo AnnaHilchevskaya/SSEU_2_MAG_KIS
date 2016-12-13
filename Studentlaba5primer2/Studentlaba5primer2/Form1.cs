d3021dce1691b9624b6ea9ea9a9d2b580bde44c9
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-13 21:38:14 +0400

    Л5. П2. Студент. Заполнение полей (без оценок)

---
+++ Studentlaba5primer2/Studentlaba5primer2/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Student
+{
+    public partial class FormStudent : Form
+    {
+        Student student1 = new Student(); // объект класса студент
+        public FormStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void textBoxFIO_TextChanged(object sender, EventArgs e)
+        {
+            student1.FIO = textBoxFIO.Text; //Сохранить фио 
+            textBoxStudentInfo.Text = student1.ToString(); 
+        }
+
+        private void textBoxNstud_TextChanged(object sender, EventArgs e)
+        {
+            student1.Nstud = textBoxNstud.Text; //сохранить номер студбилета 
+            textBoxStudentInfo.Text = student1.ToString();       
+        }
+
+        private void textGruppa_TextChanged(object sender, EventArgs e)
+        {
+            student1.gruppa = textGruppa.Text; //Сохранить группу 
+            textBoxStudentInfo.Text = student1.ToString();
+        }
+
+        private void numericUpDownKurs_ValueChanged(object sender, EventArgs e)
+        {
+            student1.kurs = (int)numericUpDownKurs.Value; //Сохранить курс 
+            textBoxStudentInfo.Text = student1.ToString();
+        }
+
+        private void radioButtonBudget_CheckedChanged(object sender, EventArgs e)
+        {
+            student1.budget = radioButtonBudget.Checked; //Сохранить бюджет.коммерцию
+            textBoxStudentInfo.Text = student1.ToString();
+
+        }
+
+       
+        
+
+       
+
+       
+    }
+}
---
+++ Studentlaba5primer2/Studentlaba5primer2/Student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Student
+{
+    class Student
+    {
+        public string FIO = "";   // ФИО
+        public string Nstud = "000000";   // № студ билета
+        public int kurs = 1;   // курс
+        public string gruppa = "";//группа
+        public bool budget = true; //бюджет
+        public byte[,] ozenki = new byte[8, 5]; //оценки
+
+        public string ToString()
+        {
+            return "ФИО: " + FIO + "\r\n" +
+                      "№ СТУДБИЛЕТА: " + Nstud + "\r\n" +
+                      "КУРС: " + kurs + "\r\n" +
+                      "ГРУППА: " + gruppa + "\r\n" +
+                      (budget ? "На бюджетной основе" : "На коммерческой основе");
+        }
+    }
+}
