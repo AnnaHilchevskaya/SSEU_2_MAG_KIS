f9f71d0494d0a598113b192fbcb6f946e24e4c2a
AnnaHilchevskaya <any4848@yandex.ru>
2016-12-14 21:58:42 +0400

    Лаба 3 Факториал

---
+++ ConsoleFaktorial/ConsoleFactorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+ using System.Collections.Generic;
+ using System.Linq;
+ using System.Text;
+ 
+ namespace ConsoleFactorial
+ {
+     class Factorial
+     {
+         static void Main(string[] args)
+         {
+             int N; //количество чисел
+             int i = 1;
+             int fuct = 1;
+             Console.WriteLine("Введите колличество чисел для вычисления факториала:");
+            if (int.TryParse(Console.ReadLine(), out N))
+             while(i < N + 1)
+             {            
+                     //факториал числа
+                     fuct = fuct * i; 
+                     Console.WriteLine("{" + fuct + "}");
+                    i++;
+             };
+             //вывод результата
+             Console.WriteLine("Факториал: " + fuct);
+             //завершение рограммы
+             Console.ReadKey();
+         }
+     }
+ }
